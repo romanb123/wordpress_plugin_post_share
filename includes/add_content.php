0f08f6a229e26cd3c05017f3d62ec342aab6a6c6
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-02-18 19:56:45 +0200

    gh

--- includes/add_content.php
+++ includes/add_content.php
@@ -4,7 +4,7 @@ function after_post_content($content)
     
     $facebook_sh = '<hr>';
     $facebook_sh .= '<div class="share">
-    <a href="//www.facebook.com/dialog/share?' .  get_the_permalink() .
+    <a href="https://www.facebook.com/sharer/sharer.php?u=' .  get_the_permalink() .
         '"><span class="dashicons dashicons-facebook">share</span></a></div>';
 
     return $content . $facebook_sh;
