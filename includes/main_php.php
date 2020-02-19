b88c3f15c8fe6c72ae43ea641bdebddc955cb23a
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-02-19 19:09:58 +0200

    gh

--- includes/main_php.php
+++ includes/main_php.php
@@ -1,7 +1,7 @@
 <?php
 function post_add_style_and_scripts()
 {
-    wp_enqueue_style('main_style', plugins_url() . '/facebook-share/css/main.css');
-    wp_enqueue_script('main_scripr', plugins_url() . '/facebook-share/javascript/main.js');
+    wp_enqueue_style('main_style', plugins_url() . '/wordpress_plugin_post_share-master/css/main.css');
+    wp_enqueue_script('main_scripr', plugins_url() . '/wordpress_plugin_post_share-master/javascript/main.js');
 }
 add_action('wp_enqueue_scripts', 'post_add_style_and_scripts');
